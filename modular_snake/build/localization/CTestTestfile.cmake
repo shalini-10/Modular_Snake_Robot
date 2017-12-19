0ea7bd819cb3412e9bb402ef6b50ce5738691392
Shalini <shaliniaddepalli@gmail.com>
2017-12-19 23:11:03 +0530

    Created the workspace modular_snake

---
+++ modular_snake/build/localization/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/shalini/projects/modular_snake/src/localization
+# Build directory: /home/shalini/projects/modular_snake/build/localization
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
